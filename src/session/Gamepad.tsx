08a7cd0d160c74dfce548dc9fb4dcc639277fa56
tutman96 <11356668+tutman96@users.noreply.github.com>
2023-10-04 09:59:40 -0400

    Fix unused variable and always build, just sometimes release

--- src/session/Gamepad.tsx
+++ src/session/Gamepad.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {gamepadStateToBuffer, useGamepadState} from '../hooks/useGamepad';
 import {InputType} from '../api';
 
