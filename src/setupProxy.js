0f574c430742b7d634eda5557877005c51894740
tutman96 <11356668+tutman96@users.noreply.github.com>
2023-10-09 19:18:08 -0400

    Fix proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 module.exports = function (app) {
   if (process.env.PROXY_HOST) {
     app.use(
-      createProxyMiddleware('/mqtt', {
+      createProxyMiddleware(['/mqtt', '/config.json'], {
         target: process.env.PROXY_HOST,
         logLevel: 'debug',
         ws: true,
